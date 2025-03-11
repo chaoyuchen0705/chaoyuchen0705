c120c00d91c031d3fb095bc88100b3c117c3b856
codeup123 <azkf@chcoo.cn>
2025-03-11 16:34:10 +0800

    update

--- src/utils/checkRedirect.js
+++ src/utils/checkRedirect.js
@@ -2,6 +2,6 @@ export default (router) => {
   const redirect = sessionStorage.getItem('redirect')
   console.log('redirect: ', redirect)
   redirect && router.push(redirect) && sessionStorage.removeItem('redirect')
-  console.log('🎯🎯🎯 router: ', JSON.parse(JSON.stringify(router)))
+  // console.log('🎯🎯🎯 router: ', JSON.parse(JSON.stringify(router)))
   console.log('router: ', router)
 }
