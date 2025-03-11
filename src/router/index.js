8684745ff331b76e46b84b79152437faa65e0402
codeup123 <azkf@chcoo.cn>
2025-03-11 11:55:19 +0800

    fix(route): 配置基础路径base，以修复网页刷新导致url片段丢失

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import ProjectsView from '../views/ProjectsView.vue'
 import ProjectDetailView from '../views/ProjectDetailView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
